2f27092306d49ba5bd3ee5ae00f394daee6b144c
pritom <chakmapritom1@gmail.com>
2024-10-03 02:43:10 +0600

    added types of video on the proper button

--- JavaScript/index.js
+++ JavaScript/index.js
@@ -1,3 +1,12 @@
+function setTime(time) {
+  const hour = parseInt(time / 3600);
+  let remainingSecong = time % 3600;
+  const munite = parseInt(remainingSecong / 60);
+  remainingSecong = remainingSecong % 60;
+
+  return `${hour} Hour ${munite} munite ${remainingSecong} second ago`;
+}
+
 // 1 . fetch , load and show catagories on html
 
 // create loadCategories
@@ -16,28 +25,56 @@ const loadVideo = () => {
     .catch((error) => console.log(error));
 };
 
-const cardDemo = {
-  category_id: "1003",
-  video_id: "aaae",
-  thumbnail: "https://i.ibb.co/Yc4p5gD/inside-amy.jpg",
-  title: "Inside Amy Schumer",
-  authors: [
-    {
-      profile_picture: "https://i.ibb.co/YD2mqH7/amy.jpg",
-      profile_name: "Amy Schumer",
-      verified: "",
-    },
-  ],
-  others: {
-    views: "3.6K",
-    posted_date: "15147",
-  },
-  description:
-    "'Inside Amy Schumer' is a comedy show by the popular comedian Amy Schumer, blending sharp satire and unfiltered humor to tackle everyday issues and societal norms. With 3.6K views, the show promises a blend of hilarious sketches, thought-provoking stand-up, and candid interviews. It's a must-watch for fans of bold, edgy comedy.",
+const loadCategoriesVideo = (id) => {
+  //   alert(id);
+  fetch(`https://openapi.programming-hero.com/api/phero-tube/category/${id}`)
+    .then((res) => res.json())
+    .then((data) => displayVideos(data.category))
+    .catch((error) => console.log(error));
 };
+
+// const cardDemo = {
+//   category_id: "1003",
+//   video_id: "aaae",
+//   thumbnail: "https://i.ibb.co/Yc4p5gD/inside-amy.jpg",
+//   title: "Inside Amy Schumer",
+//   authors: [
+//     {
+//       profile_picture: "https://i.ibb.co/YD2mqH7/amy.jpg",
+//       profile_name: "Amy Schumer",
+//       verified: "",
+//     },
+//   ],
+//   others: {
+//     views: "3.6K",
+//     posted_date: "15147",
+//   },
+//   description:
+//     "'Inside Amy Schumer' is a comedy show by the popular comedian Amy Schumer, blending sharp satire and unfiltered humor to tackle everyday issues and societal norms. With 3.6K views, the show promises a blend of hilarious sketches, thought-provoking stand-up, and candid interviews. It's a must-watch for fans of bold, edgy comedy.",
+// };
 
 const displayVideos = (videos) => {
   const videoContainer = document.getElementById("video");
+  videoContainer.innerHTML = " ";
+
+  if (videos.length === 0) {
+    videoContainer.classList.remove("grid");
+    videoContainer.innerHTML = `
+    <div class="min-h-sceen w-full flex flex-col gap-5 justify-center items-center ">
+    <img src="assest/Icon.png"  />
+
+    <h2 class="text-xl font-bold text-center">
+    Oopps!! Sorry, There is no <br />
+    Content Here 
+    </h2>
+    </div>
+    
+    `;
+    return;
+  } else {
+    videoContainer.classList.add("grid");
+  }
+
   videos.forEach((video) => {
     console.log(video);
     // create card
@@ -53,7 +90,9 @@ const displayVideos = (videos) => {
 ${
   video.others.posted_date?.length === 0
     ? " "
-    : `<span class="absolute right-2 bottom-2 bg-black text-white rounded p-1">${video.others.posted_date}</span>`
+    : `<span class="absolute text-xs right-2 bottom-2 bg-black text-white rounded p-1">${setTime(
+        video.others.posted_date
+      )}</span>`
 }
 
       
@@ -78,31 +117,12 @@ ${
     ? `<img class="w-5 h-5 object-cover" src="https://img.icons8.com/?size=96&id=D9RtvkuOe31p&format=png" />`
     : " "
 }
-
-
-
-
-</div>
-
-<p></p>
-
-</div>
-
-
-
- 
   </div>
     `;
     videoContainer.append(card);
   });
 };
-// category
-// :
-// "Music"
-// category_id
-// :
-// "1001"
-// create DisplayCategories
+
 const DisplayCategories = (categories) => {
   const categoriesContainer = document.getElementById("categories");
 
@@ -110,12 +130,12 @@ const DisplayCategories = (categories) => {
     console.log(item);
 
     // create a button
-    const button = document.createElement("button");
-    button.classList = "btn";
-    button.innerText = item.category;
-
+    const buttonContainer = document.createElement("div");
+    buttonContainer.innerHTML = `
+    <button onclick=" loadCategoriesVideo(${item.category_id})" class="btn">${item.category}</button>
+    `;
     // add button to categories container
-    categoriesContainer.append(button);
+    categoriesContainer.append(buttonContainer);
   });
 };
 
